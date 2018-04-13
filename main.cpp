c8becd02a3e8d6f9dd0213f6fbbe7944388e0538
Jason A. Tran <jasonatran@gmail.com>
2018-04-13 12:56:46 -0700

    add m3pi movement code

--- main.cpp
+++ main.cpp
@@ -47,6 +47,7 @@
  */
 
 #include "mbed.h"
+#include "m3pi.h"
 #include "easy-connect.h"
 #include "MQTTmbed.h"
 #include "MQTTNetwork.h"
@@ -68,7 +69,13 @@ extern "C" void mbed_reset();
 #define EASY_CONNECT_LOGGING    true
 
 DigitalOut wifiHwResetPin(WIFI_HW_RESET_PIN);
-InterruptIn pushbutton(p8);
+
+/** Initialize the m3pi for robot movements. There is an atmega328p MCU in the
+ *  3pi robot base. It's UART lines are connected to the LPC1768's p9 and p10.
+ *  If you send the right sequence of UART characters to the atmega328p, it will
+ *  move the robot for you. We provide a movement() function below for you to use
+ */
+m3pi m3pi(p23, p9, p10);
 
 /* MQTTClient and TCPSocket (underneath MQTTNetwork) may not be thread safe. 
  * Lock this global mutex before any calls to publish(). 
@@ -76,6 +83,50 @@ InterruptIn pushbutton(p8);
 Mutex mqttMtx;
 
 static char *topic = "m3pi-mqtt-ee250";
+
+/**
+ * @brief      controls movement of the 3pi
+ *
+ *  If you want to use this function in a file outside of main.cpp, the easiest
+ *  way to get access to it is to add
+ *      
+ *      extern movement(char command, char speed, int delta_t)
+ *
+ * in the .cpp file in which you want to use it.
+ *
+ * @param[in]  command  The movement command
+ * @param[in]  speed    The speed of the movement (start by trying 25)
+ * @param[in]  delta_t  The time for each movement in msec (start by trying 100)
+ *
+ * @return     { void }
+ */
+void movement(char command, char speed, int delta_t)
+{
+    if (command == 's')
+    {
+        m3pi.forward(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }    
+    else if (command == 'a')
+    {
+        m3pi.left(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }   
+    else if (command == 'w')
+    {
+        m3pi.backward(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }
+    else if (command == 'd')
+    {
+        m3pi.right(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }
+}
 
 /* Callback for any received MQTT messages */
 void messageArrived(MQTT::MessageData& md)
@@ -130,6 +181,28 @@ void messageArrived(MQTT::MessageData& md)
 
 int main()
 {
+    /* Uncomment this to see how the m3pi moves. This sequence of functions
+       represent w-a-s-d like controlling. Each button press moves the robot
+       at a speed of 25 (speed can be between -127 to 127) for 100 ms. Use
+       functions like this in your program to move your m3pi when you get 
+       MQTT messages! */
+    // movement('w', 25, 100);
+    // movement('w', 25, 100);
+    // movement('w', 25, 100);
+    // movement('w', 25, 100);
+    // movement('a', 25, 100);
+    // movement('a', 25, 100);
+    // movement('a', 25, 100);
+    // movement('a', 25, 100);
+    // movement('d', 25, 100);
+    // movement('d', 25, 100);
+    // movement('d', 25, 100);
+    // movement('d', 25, 100);
+    // movement('s', 25, 100);
+    // movement('s', 25, 100);
+    // movement('s', 25, 100);
+    // movement('s', 25, 100);
+
     wait(1); //delay startup 
     printf("Resetting ESP8266 Hardware...\n");
     wifiHwResetPin = 0;
