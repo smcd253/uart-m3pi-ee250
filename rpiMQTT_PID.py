0781cc5f110661bee0299646866022890ec28c7a
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2018-04-23 20:42:41 -0700

    pid controller

--- rpiMQTT_PID.py
+++ rpiMQTT_PID.py
@@ -123,13 +123,13 @@ if __name__ == '__main__':
 
         print("control = " + str(control)
         # decide left/right
-        if control < 0: # right
-            right.append(control)
-            client.publish("m3pi-mqtt-ee250", right)
-        elif control > 0: #left
-            left.append(control)
-            client.publish("m3pi-mqtt-ee250", left)
-        else: #centered
-            client.publish("m3pi-mqtt-ee250", stop)
+        # if (control < 0): # right
+        #     right.append(control)
+        #     client.publish("m3pi-mqtt-ee250", right)
+        # elif (control > 0): #left
+        #     left.append(control)
+        #     client.publish("m3pi-mqtt-ee250", left)
+        # else: #centered
+        #     client.publish("m3pi-mqtt-ee250", stop)
 
 
