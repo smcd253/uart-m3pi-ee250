9e72b8a6b588a0671df4e90a17828f0119b884dc
Spencer McDonough <SwaggySpencerMcDee@Spencers-MacBook-Air-3.local>
2018-05-03 14:16:27 -0700

    get uart working

--- main.cpp
+++ main.cpp
@@ -48,41 +48,18 @@
 
 #include "mbed.h"
 #include "m3pi.h"
-#include "easy-connect.h"
-#include "MQTTmbed.h"
-#include "MQTTNetwork.h"
-#include "MQTTClient.h"
-#include "MailMsg.h"
-#include "LEDThread.h"
-#include "PrintThread.h"
+
 
 extern "C" void mbed_reset();
 
-/* connect this pin to both the CH_PD (aka EN) & RST pins on the ESP8266 just in case */
-#define WIFI_HW_RESET_PIN       p26
-
-/* Using a hostname instead of IP address has been unverified by us */
-#define MQTT_BROKER_IPADDR      "128.125.124.160"  // eclipse.usc.edu == 128.125.124.160
-#define MQTT_BROKER_PORT        11000
-
-/* turn on easy-connect debug prints */
-#define EASY_CONNECT_LOGGING    true
-
-DigitalOut wifiHwResetPin(WIFI_HW_RESET_PIN);
 
 /** Initialize the m3pi for robot movements. There is an atmega328p MCU in the
  *  3pi robot base. It's UART lines are connected to the LPC1768's p9 and p10.
  *  If you send the right sequence of UART characters to the atmega328p, it will
  *  move the robot for you. We provide a movement() function below for you to use
  */
-// m3pi m3pi(p23, p9, p10);
+m3pi m3pi(p23, p9, p10);
 
-/* MQTTClient and TCPSocket (underneath MQTTNetwork) may not be thread safe. 
- * Lock this global mutex before any calls to publish(). 
- */
-Mutex mqttMtx;
-
-static char *topic = "m3pi-mqtt-ee250";
 
 /**
  * @brief      controls movement of the 3pi
@@ -100,88 +77,47 @@ static char *topic = "m3pi-mqtt-ee250";
  *
  * @return     { void }
  */
-// void movement(char command, char speed, int delta_t)
-// {
-//     if (command == 's')
-//     {
-//         m3pi.forward(speed);
-//         Thread::wait(delta_t);
-//         m3pi.stop();
-//     }    
-//     else if (command == 'a')
-//     {
-//         m3pi.left(speed);
-//         Thread::wait(delta_t);
-//         m3pi.stop();
-//     }   
-//     else if (command == 'w')
-//     {
-//         m3pi.backward(speed);
-//         Thread::wait(delta_t);
-//         m3pi.stop();
-//     }
-//     else if (command == 'd')
-//     {
-//         m3pi.right(speed);
-//         Thread::wait(delta_t);
-//         m3pi.stop();
-//     }
-// }
-
-/* Callback for any received MQTT messages */
-void messageArrived(MQTT::MessageData& md)
+void movement(char command, char speed, int delta_t)
 {
-    MQTT::Message &message = md.message;
-    MailMsg *msg;
-
-    /* our messaging standard says the first byte denotes which thread to 
-       forward the packet payload to */
-    char fwdTarget = ((char *)message.payload)[0];
-
-
-    /* Ship (or "dispatch") the entire message via Mail to threads since the 
-       reference to messages will be destroyed by the MQTT thread when this 
-       callback returns */
-    switch(fwdTarget)
+    if (command == 's')
     {
-        case FWD_TO_PRINT_THR:
-            printf("fwding to print thread\n");
-
-            /* allocate the memory for a piece of mail */
-            msg = getPrintThreadMailbox()->alloc();
-
-            if (!msg) {
-                printf("print thread mailbox full!\n");
-                break;
-            }
-
-            /* copy the message into the newly allocated MailMsg struct */
-            memcpy(msg->content, message.payload, message.payloadlen);
-            msg->length = message.payloadlen;
-
-            /* put the piece of mail into the target thread's mailbox */
-            getPrintThreadMailbox()->put(msg);
-            break;
-        case FWD_TO_LED_THR:
-            printf("fwding to led thread\n");
-            msg = getLEDThreadMailbox()->alloc();
-            if (!msg) {
-                printf("led thread mailbox full!\n");
-                break;
-            }
-            memcpy(msg->content, message.payload, message.payloadlen);
-            msg->length = message.payloadlen;
-            getLEDThreadMailbox()->put(msg);
-            break;
-        default:
-            /* do nothing */
-            printf("Unknown MQTT message\n");
-            break;
+        m3pi.forward(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }    
+    else if (command == 'a')
+    {
+        m3pi.left(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }   
+    else if (command == 'w')
+    {
+        m3pi.backward(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
+    }
+    else if (command == 'd')
+    {
+        m3pi.right(speed);
+        Thread::wait(delta_t);
+        m3pi.stop();
     }
 }
 
+
+//uart
+void callback() {
+    // Note: you need to actually read from the serial to clear the RX interrupt
+    printf("%c\n", pc.getc());
+}
+Serial rpi(p13, p14);  // tx, rx
+
 int main()
 {
+    // attach uart interrupt
+    rpi.attach(&callback);
+
     /* Uncomment this to see how the m3pi moves. This sequence of functions
        represent w-a-s-d like controlling. Each button press moves the robot
        at a speed of 25 (speed can be between -127 to 127) for 100 ms. Use
@@ -204,85 +140,8 @@ int main()
     // movement('s', 25, 100);
     // movement('s', 25, 100);
 
-    wait(1); //delay startup 
-    printf("Resetting ESP8266 Hardware...\n");
-    wifiHwResetPin = 0;
-    wait(1);
-    wifiHwResetPin = 1;
-
-    printf("Starting MQTT example with an ESP8266 wifi device using Mbed OS.\n");
-    printf("Attempting to connect to access point...\n");
-
-    /* wifi ssid/pw and wifi interface settings are set in mbed_app.json. 
-     * NetworkInterface is mbed-OS's abstraction of any network interface */
-    NetworkInterface* wifi = easy_connect(EASY_CONNECT_LOGGING);
-    if (!wifi) {
-        printf("Connection error! Your ESP8266 may not be responding.\n");
-        printf("Try double checking your circuit or unplug/plug your LPC1768 board.\n");
-        printf("Exiting.\n");
-        return -1;
-    }
-
-    const char *ipAddr = wifi->get_ip_address();
-    printf("Success! My IP addr: %s\n", ipAddr);
-    char clientID[30];
-    /* use ip addr as unique client ID */
-    strcpy(clientID, ipAddr);
-
-    /* wrapper for (NetworkInterface *wifi) to adapt to MQTTClient.h */
-    MQTTNetwork mqttNetwork(wifi);
-    MQTT::Client<MQTTNetwork, Countdown> client(mqttNetwork);
-
-    printf("Connecting to %s:%d\n", MQTT_BROKER_IPADDR, MQTT_BROKER_PORT);
-    int retval = mqttNetwork.connect(MQTT_BROKER_IPADDR, MQTT_BROKER_PORT);
-    if (retval != 0)
-        printf("TCP connect returned %d\n", retval);
-
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;   //support only available up to ver. 3
-    // data.keepAliveInterval = 60; //MQTTPacket_connectData_initializer sets this to 60
-    data.clientID.cstring = clientID; 
-    if ((retval = client.connect(data)) != 0)
-        printf("connect returned %d\n", retval);
-
-
-    /* define MQTTCLIENT_QOS2 as 1 to enable QOS2 (see MQTTClient.h) */
-    /* This call attaches the messageArrived callback to handle MQTT messages 
-       that arrive */
-    if ((retval = client.subscribe(topic, MQTT::QOS0, messageArrived)) != 0)
-        printf("MQTT subscribe returned %d\n", retval);
-
-    /* This is a good point to launch your threads. If you want to create 
-       another thread, you can look at the structure of the two threads we 
-       provided and make a copy of it. Otherewise, you can gut out the two 
-       threads and insert your application code. Read the LEDThread and 
-       PrintThread files to understand how these threads work.*/
-    Thread ledThr;
-    Thread printThr;
-
-    /* Here, we pass in a pointer to the MQTT client so the LED thread can 
-       client.publish() messages */
-    ledThr.start(callback(LEDThread, (void *)&client));
-
-    /* Here, we do not pass the pointer in. This means the printing thread 
-       won't be able to publish any MQTT messages. Modify this accordingly if
-       you need to publish. */
-    printThr.start(printThread);
-
-    /* The main thread will now run in the background to keep the MQTT/TCP 
-     connection alive. MQTTClient is not an asynchronous library. Paho does
-     have MQTTAsync, but some effort is needed to adapt mbed OS libraries to
-     be used by the MQTTAsync library. Please do NOT do anything else in this
-     thread. Let it serve as your background MQTT thread. */
-    while(1) {
-        Thread::wait(1000);
-        printf("main: yielding...\n", client.isConnected());
-
-        if(!client.isConnected())
-            mbed_reset(); //connection lost! software reset
-
-        /* yield() needs to be called at least once per keepAliveInterval. */
-        client.yield(1000);
+    while(1){
+        // waiting for serial from rpi
     }
 
     return 0;
