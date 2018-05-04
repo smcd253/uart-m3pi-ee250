9d5a1ad2261895de1a015a116c71f6ce0756c9e4
Spencer McDonough <SwaggySpencerMcDee@Spencers-Air-3.lan>
2018-05-03 17:36:31 -0700

    get uart working

--- rpi_serial_uart.py
+++ rpi_serial_uart.py
@@ -17,7 +17,7 @@ print("starting")
 while True:
     time.sleep(1)
     print("sending synch")
-    A = bytearray('A')
+    A = bytearray('A', 'utf-8')
     ser.write(A)
     ser.write('\n')
     # rcv = readLine(ser)
